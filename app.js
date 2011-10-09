9cf2031df2e7f6e75bec34e1de30ae2f36978596
Ben Buckman, New Leaf Digital <ben@newleafdigital.com>
2011-10-08 23:42:36 -0300

    cleanup

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
 // Spanish Flashcards app -- learning node.js & express.js with mongodb
 
 // @todo figure out which of these are really necessary
-var express = require('express'),
-    app = module.exports = express.createServer(),
-    // messages = require('express-messages'),
-    messages = require('./messages'),
-    // events = require('events'),
-    sys = require('sys')
-    // util = require('util')
-    // fs = require('fs'), //?
-    // parse = require('url').parse,
-    // forms = require('forms'),
-    // fields = forms.fields,    // all needed here?
-    // widgets = forms.widgets,
-    // validators = forms.validators,
-    // _ = require('underscore')._   //,
-    // wordModel = require('./models/word')
+var express = require('express')
+    , app = module.exports = express.createServer()
+    , messages = require('./messages')   // [modified from lib]
+    //, sys = require('sys')
     ;
 
 // [quasi] models
 // require('./models/word');
 
-// global vars -- [is this bad form?]
-// global.dbName = 'flashcards';
-// global.wordsCollection = 'words';
 global.appTitle = 'Ben\'s Spanish Flashcards';
 
 global.wordLanguages = {
   "en": "English",
   "es": "Spanish"
 };
-
-// instantiate a db/model handler
-// var flashcardHandler = new FlashcardHandler();
 
 
 // [what's the difference between putting here or inside app.configure() ??]
@@ -83,91 +66,6 @@ require('./routes/home.js')(app);
 require('./routes/word.js')(app);
 require('./routes/play.js')(app);
 
-
-// // form page loaded (not submitted)
-// app.get('/add', function(req, res) {
-//   var wordForm = new WordForm();
-//   wordForm.render(req, res, {});
-// });
-
-
-// // form submitted
-// app.post('/add', function(req, res) {
-//   var wordForm = new WordForm();
-//   wordForm.handle(req, res, flashcardHandler);
-// });
-
-// // (edit form goes to /add path w/ ID in form)
-// app.get('/edit/:id', function(req, res) {
-//   var id = req.param('id');
-//   flashcardHandler.getById( global.wordsCollection, id, function(error, results) {
-//     if (error) return app.prettyError(error, req, res);
-//     
-//     var word = new WordModel(results);
-//     
-//     var wordForm = new WordForm();
-//     wordForm.render(req, res, {
-//       edit: word
-//     });
-//   });
-// });
-
-// // edit via post same as via get
-// app.post('/edit/:id', function(req, res) {
-//   var id = req.param('id');
-//   flashcardHandler.getById( global.wordsCollection, id, function(error, results) {
-//     if (error) return app.prettyError(error, req, res);
-//     
-//     var word = new WordModel(results);
-//     console.log('modeled word to edit: ', word);
-//     
-//     var wordForm = new WordForm();
-//     wordForm.handle(req, res, flashcardHandler);
-//     
-//     
-//     // GETTING ALL CONFUSED WITH THE handle() and render() ... this shouldn't be so complicated!!!!    
-//   });
-// });
-
-
-
-// -- /list moved to routes/word.js
-
-
-// app.get('/delete/:id', function(req, res) {
-//   var id = req.param('id');
-//   flashcardHandler.remove( global.wordsCollection, id, function(error, results) {
-//     if (error) app.prettyError(error, req, res);
-//     res.render('index', {
-//       pageTitle: 'Delete',
-//       debug: 'ID: ' + id + '<br/>' + util.inspect(results)
-//     });
-//   });
-// });
-
-
-
-// catch everything else
-/*app.all('*', function(req, res, next) {
-  res.render('error', {
-    pageTitle: 'Not Found',
-    error: 'The requested page does not exist.'
-  });
-});
-*/
-
-// @todo find a more express-y way to do this. -- flash messages?
-app.prettyError = function(error, req, res) {
-  res.render('error', {
-    pageTitle: 'An error occurred',
-    error: error      // (assume string)
-  });
-}
-
-// test error
-app.get('/error', function(req, res){
-  app.prettyError({'msg':'A fake error'}, req, res);
-});
 
 if (!module.parent) {
   app.listen(3000);
